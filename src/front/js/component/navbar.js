ebe89543d6596f0a33cdd248b4cf7e3c9a22070a
Rick <77856968+Ricardo-Patino@users.noreply.github.com>
2021-05-05 21:37:43 -0600

    Update navbar.js

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,15 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../img/logo.jpeg";
+import { Search } from "./search";
+import "../../styles/home.scss";
+import "../../styles/index.scss";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-verde mb-3 verde">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img src={"logo.jpeg"} style={{ width: "80px", height: "40px" }} />
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+			<Search />
+			<div className="ml-auto nav">
+				<Link className="nav-item" to="/RegisterUserIn">
+					<a
+						className="nav-link"
+						style={{ color: "white", background: "#0", fontFamily: "'Roboto', sans-serif" }}>
+						Reservar
+					</a>
+				</Link>
+
+				<Link className="nav-item" to="/logUserIn">
+					<a
+						className="nav-link"
+						style={{ color: "white", background: "#0", fontFamily: "'Roboto', sans-serif" }}>
+						Fotos
+					</a>
+				</Link>
+
+				<Link className="nav-item" to="/contact-us">
+					<a
+						className="nav-link"
+						style={{ color: "white", background: "#0", fontFamily: "'Roboto', sans-serif" }}>
+						Contacto
+
+					</a>
 				</Link>
 			</div>
 		</nav>
